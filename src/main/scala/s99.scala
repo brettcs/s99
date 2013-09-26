15ae5f0c48d4174c4f8b7daa891f422a1cc3e1c8
Brett Smith <brett@w3.org>
2013-09-26 15:52:35 -0400

    P02: First solution.

--- src/main/scala/s99.scala
+++ src/main/scala/s99.scala
@@ -6,4 +6,11 @@ object S99 {
       case _ :: tail => last(tail)
     }
   }
+
+  def penultimate[A](list: List[A]): A = list match {
+    case Nil => throw new NoSuchElementException
+    case _ :: Nil => throw new NoSuchElementException
+    case x :: _ :: Nil => x
+    case _ :: tail => penultimate(tail)
+  }
 }
--- src/test/scala/s99Suite.scala
+++ src/test/scala/s99Suite.scala
@@ -6,4 +6,14 @@ class S99Spec extends FlatSpec {
   "Function last" should "return the last element of a list" in {
     1 to 10 foreach(max => assert(last((1 to max).toList) === max))
   }
+
+  "Function penultimate" should "return the second-last element of a list" in {
+    2 to 10 foreach(max => assert(penultimate((1 to max).toList) === (max - 1)))
+  }
+
+  it should "throw an exception for a too-short list" in {
+    intercept[NoSuchElementException] {
+      penultimate(List(1))
+    }
+  }
 }
